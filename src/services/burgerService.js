828e4ba127f3cd24ad2fec8fe3ec180544a736ba
PabloRoger <parovi97@gmail.com>
2023-04-24 20:26:35 +0200

    new empty burgerService

---
+++ src/services/burgerService.js
@@ -0,0 +1,19 @@
+const getAllBurgers = () =>{
+  return;
+}
+
+const getBurgerById = () =>{
+  return;
+}
+
+const createBurger = () =>{
+  return;
+}
+
+const updateBurger = () =>{
+  return;
+}
+
+const deleteBurger = () =>{
+  return;
+}
