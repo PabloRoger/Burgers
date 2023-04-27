b476c7629fcc811868847a6d7d62b9910346057e
PabloRoger <parovi97@gmail.com>
2023-04-27 20:23:44 +0200

    Update main path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const v1BurgerRoutes = require("./v1/routes/burgersRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1/burgers", v1BurgerRoutes);
+app.use("/api/v1", v1BurgerRoutes);
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
