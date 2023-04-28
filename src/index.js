628b2b8d326bc5dc226a001f7c1e63c89e409f40
PabloRoger <parovi97@gmail.com>
2023-04-28 20:34:54 +0200

    comment explaining express.json

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const v1BurgerRoutes = require("./v1/routes/burgersRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware needed to parse JSON bodies of POST requests
+app.use(express.json());
 app.use("/api/v1", v1BurgerRoutes);
 
 app.listen(PORT, () => {
