01e96cc981bf766997a6d71cdbe709c7500d9953
PabloRoger <parovi97@gmail.com>
2023-04-30 16:31:59 +0200

    Set up static files + handdlebars + index route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const path = require("path");
 const v1BurgerRoutes = require("./v1/routes/burgersRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "./public");
 
+// Set up static files
+app.use(express.static(publicPath));
 // Middleware needed to parse JSON bodies of POST requests
 app.use(express.json());
+// API Rest routes
 app.use("/api/v1", v1BurgerRoutes);
 
+// Set up handlebars
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "hbs");
+
+// Set up routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`)
+    console.log(`Server running on port ${PORT}!`)
 });
